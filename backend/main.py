e2d8ae1297a222b08c977f831f0e252d4056cdf9
Junot974 <antoine.junotpro@gmail.com>
2025-11-02 15:22:47 +0100

    Initial dockerized version

--- backend/main.py
+++ backend/main.py
@@ -479,11 +479,11 @@ async def analyze_stock(input_str: str):
 
 
 # === Point d'entrée de l'application ===
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(
-        "main:app",
-        host="0.0.0.0",
-        port=8000,
-        reload=True
-    )
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(
+#         "main:app",
+#         host="0.0.0.0",
+#         port=8000,
+#         reload=True
+#     )
