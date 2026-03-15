ee1c6f2258dd2f4d7180b0ca80fc71350f9371b6
Junot974 <antoine.junotpro@gmail.com>
2026-03-15 12:19:02 +0100

    [FIXED] Some issues

--- stock-dashboard/vite.config.js
+++ stock-dashboard/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  build: {
+    modulePreload: { polyfill: false },
+  },
   server: {
     port: 3000
   },
